5d51730c5c1dedc1ab4b5732d0ff117e08d0155f
mhkejade <michaeljade.bengil@devpartners.co>
2020-03-27 23:02:21 +0800

    Add files via upload

---
+++ Kettle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EletricKettle
+{
+    public sealed class Kettle
+    {
+        Kettle() { }
+
+        private static Kettle _kettleinstance = null;
+        private static readonly object _lock = new object();
+        public State _kettlestate { get; set; } = State.Empty;
+
+        public static Kettle Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_kettleinstance == null)
+                    {
+                        _kettleinstance = new Kettle();
+                    }
+                    return _kettleinstance;
+                }
+            }
+        }
+        
+
+        public void Fill()
+        {
+            if (_kettlestate == State.Empty)
+            {
+                Console.WriteLine("Filling...\n");
+                _kettlestate = State.InProgress;
+            }
+        }
+
+        public void Drain()
+        {
+            if (_kettlestate != State.InProgress)
+                Console.WriteLine("Draining...\n");
+            _kettlestate = State.Empty;
+        }
+
+        public void Boil()
+        {
+            if (_kettlestate == State.InProgress)
+                Console.WriteLine("Boiling...\n");
+            _kettlestate = State.Boiled;
+        }
+    }
+
+    public enum State
+    {
+        Empty,
+        InProgress,
+        Boiled
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace EletricKettle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting...\n");
+            Thread.Sleep(1000);
+
+            Kettle.Instance.Fill();
+            Thread.Sleep(1000);
+
+            Kettle.Instance.Boil();
+            Thread.Sleep(1000);
+
+            Kettle.Instance.Drain();
+            Thread.Sleep(1000);
+
+            Console.ReadLine();
+        }
+    }
+}
